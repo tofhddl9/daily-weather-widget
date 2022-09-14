54ab108e2de6b0c82b4ae74b4370698c0c5a5862
tofhddl9 <tofhddl9@gmail.com>
2022-09-15 00:09:34 +0900

    Polishing UI

--- app/src/main/java/com/lgtm/daily_weather_widget/presentation/WeatherFragment.kt
+++ app/src/main/java/com/lgtm/daily_weather_widget/presentation/WeatherFragment.kt
@@ -1,10 +1,15 @@
 package com.lgtm.daily_weather_widget.presentation
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -27,6 +32,16 @@ class WeatherFragment: Fragment(R.layout.fragment_weather) {
 
     private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
 
+    private val requestBackgroundLocation = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            showSnackBar("이제, 앱 위젯을 사용할 수 있습니다.")
+        } else {
+            showSnackBar("앱 위젯을 사용하고 싶다면, 나중에 다시 변경해주세요.")
+        }
+    }
+
     private val binding: FragmentWeatherBinding by viewBinding(FragmentWeatherBinding::bind)
 
     private val viewModel: WeatherViewModel by viewModels()
@@ -48,10 +63,15 @@ class WeatherFragment: Fragment(R.layout.fragment_weather) {
             viewModel.getCurrentWeather()
         }
 
-        permissionLauncher.launch(arrayOf(
+        val permissionList = arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-        ))
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        permissionLauncher.launch(permissionList)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            requireContext().checkBackgroundLocationPermission()
+        }
     }
 
     private fun observeViewModel() {
@@ -85,7 +105,6 @@ class WeatherFragment: Fragment(R.layout.fragment_weather) {
 
     }
 
-
     private fun initToolbar() = with(binding.toolbar) {
         inflateMenu(R.menu.weather_menu)
         setOnMenuItemClickListener {
@@ -103,6 +122,26 @@ class WeatherFragment: Fragment(R.layout.fragment_weather) {
 
     private fun moveToSetting() {
         findNavController().navigate(WeatherFragmentDirections.actionWeatherFragmentToSettingFragment())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun Context.checkBackgroundLocationPermission() {
+        if (checkSinglePermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION)) return
+        AlertDialog.Builder(this)
+            .setTitle("위치 권한 설정 안내")
+            .setMessage("앱 위젯을 사용하고 싶다면, 위치 권한을 \"항상 허용\"으로 설정해주세요")
+            .setPositiveButton("설정하기") { _, _ ->
+                requestBackgroundLocation.launch(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+            }
+            .setNegativeButton("닫기") { dialog,_ ->
+                dialog.dismiss()
+            }
+            .create()
+            .show()
+    }
+
+    private fun Context.checkSinglePermission(permission: String) : Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
     }
 
 }
